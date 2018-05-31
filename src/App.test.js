b505a7c3b770163deec400c7bb25f990f5f9629d
budarin <budarin-vv@yandex.ru>
2018-05-31 19:05:36 +0300

    add jest & puppeteer

--- src/App.test.js
+++ src/App.test.js
@@ -6,22 +6,3 @@ it('renders correctly', () => {
     const tree = renderer.create(<App />).toJSON();
     expect(tree).toMatchSnapshot();
 });
-
-const timeout = 10000;
-
-describe(
-    '/ (Home Page)',
-    () => {
-        let page;
-        beforeAll(async () => {
-            page = await global.browser.newPage();
-            await page.goto('https://hsrv');
-        }, timeout);
-
-        it('should load without error', async () => {
-            const text = await page.evaluate(() => document.body.textContent);
-            expect(text).toContain('google');
-        });
-    },
-    timeout,
-);
