a0e3914354561382569f6eafa0b4d0b93e295cb4
budarin <budarin-vv@yandex.ru>
2018-06-01 16:02:18 +0300

    tests modifications

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import App from './App';
 
-it('renders correctly', () => {
+test('renders correctly', () => {
     const tree = renderer.create(<App />).toJSON();
     expect(tree).toMatchSnapshot();
+});
+
+test('renders p', () => {
+    const app = shallow(<App />);
+    expect(app.find('p')).toHaveLength(1);
 });
