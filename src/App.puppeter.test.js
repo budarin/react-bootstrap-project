5d5a340a01d8296652424f8755648f1a7bdadadd
budarin <budarin-vv@yandex.ru>
2018-06-01 15:24:56 +0300

    upgrade

--- src/App.puppeter.test.js
+++ src/App.puppeter.test.js
@@ -1,23 +1,16 @@
+let page;
 const timeout = 10000;
 
-describe(
-    'Home Page test',
-    () => {
-        let page;
+beforeAll(async () => {
+    page = await global.browser.newPage();
+    await page.goto('https://localhost/');
+}, timeout);
 
-        beforeAll(async () => {
-            page = await global.browser.newPage();
-            await page.goto('https://localhost/');
-        }, timeout);
+afterAll(async () => {
+    await page.close();
+});
 
-        afterAll(async () => {
-            await page.close();
-        });
-
-        it('should load without error', async () => {
-            const text = await page.evaluate(() => document.body.textContent);
-            expect(text).toContain('Hello World!');
-        });
-    },
-    timeout,
-);
+it('should load without error', async () => {
+    const text = await page.evaluate(() => document.body.textContent);
+    expect(text).toContain('Hello World!');
+});
