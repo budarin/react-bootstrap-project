91cecb4eaa0a09564c5d986626b1c2c36c295976
budarin <budarin-vv@yandex.ru>
2018-06-20 20:50:17 +0300

    remove flow

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// @flow
 import React from 'react';
 import app from './app.css';
 
@@ -9,7 +8,6 @@ function App() {
     return <p className={css.hello}>Hello World!</p>;
 }
 
-// Demo of working flow
 function a(b) {
     return b.length;
 }
