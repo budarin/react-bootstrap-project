e4b87d76982170ba7fa412196b814906e48b9bb8
budarin <budarin-vv@yandex.ru>
2018-06-22 17:40:52 +0300

    fix

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,8 +1,12 @@
 // декларируем любой .css модуль который будет возвращать any
-declare module '*.css' {
-    export const use: Function;
-    export const unuse: Function;
-    export const locals: {
+export interface CssModule {
+    use: Function;
+    unuse: Function;
+    locals: {
         [name: string]: string;
     };
+}
+
+declare module '*.css' {
+    export interface CssModule {}
 }
