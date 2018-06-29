5f6406469807a11a4351d423ca00473efa327e26
budarin <budarin-vv@yandex.ru>
2018-06-29 16:57:32 +0300

    upgrade

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { hot } from 'react-hot-loader';
 import app from './app.css';
 
 class App extends React.Component {
-    render() {
+    public render() {
         const css = app.locals;
         app.use();
         return <p className={css.hello}>Hello World!</p>;
